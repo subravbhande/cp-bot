eac77d8a6ed31684d979a63117982f23a2df3861
wildwarrior_44 <119719959+Vishal490404@users.noreply.github.com>
2026-02-09 23:12:59 +0530

    Update ids.js

--- ids.js
+++
@@ -1,9 +0,0 @@
-export const IDS = [
-    // '120363045640332810@g.us', // CWC Community(700)
-    // '120363317354530397@g.us', // -> CWC MAIN + ASSIS
-    '919322512338@s.whatsapp.net',
-    // '919503030395@s.whatsapp.net',
-    '120363055841239377@g.us', // Testing
-    // '917517756075@s.whatsapp.net',
-    // '917038815102@s.whatsapp.net',
-]
